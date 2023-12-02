4d6ce9771cdf42b4c0685d6862d122075b81585b
Emma Luk <emma_luk1@hotmail.com>
2023-12-02 17:18:25 +0000

    get legacy_users data

--- Python/main.py
+++ Python/main.py
@@ -1,20 +1,16 @@
 from data_extraction import DataExtractor
 from database_utils import DatabaseConnector
-from data_cleaning import DataCleaning
 
 # Extract data from RDS database
 data_extractor = DataExtractor()
 db_connector = DatabaseConnector()
 sourceDB = db_connector.init_db_engine()
-sourceList = db_connector.list_db_tables() #  list_db_tables 
+
+# Get legacy users table name
 table_name = 'legacy_users'
 
-user_data = data_extractor.extract_data_from_rds(db_connector, table_name)
-# user_data = data_extractor.read_rds_table(table_name, sourceDB)
+# Extract legacy users data
+legacy_users_data = data_extractor.read_rds_table(table_name, sourceDB)
 
-# Clean user data
-#data_cleaning = DataCleaning()
-#cleaned_user_data = data_cleaning.clean_user_data(user_data)
-
-# Upload cleaned data to database
-#db_connector.upload_to_db(cleaned_user_data, 'dim_users')
+# Print legacy users data
+print(legacy_users_data)
