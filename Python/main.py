612a246a63818e5246975eae405664adbd3b84e2
Emma Luk <emma_luk1@hotmail.com>
2023-12-05 11:16:48 +0000

    clean legacy_users_data

--- Python/main.py
+++ Python/main.py
@@ -18,12 +18,12 @@ print(legacy_users_data)
 # Extract data from RDS database
 # data_extractor = DataExtractor()
 # db_connector = DatabaseConnector()
-table_name = 'user_data'
-user_data = data_extractor.extract_data_from_rds(table_name)
+## table_name = 'user_data'
+## user_data = data_extractor.extract_data_from_rds(table_name)
 
 # Clean user data
 data_cleaning = DataCleaning()
-cleaned_user_data = data_cleaning.clean_user_data(user_data)
+cleaned_user_data = data_cleaning.clean_user_data(legacy_users_data)
 
 # Upload cleaned data to database
 db_connector.upload_to_db(cleaned_user_data, 'dim_users', 'sales_data')
