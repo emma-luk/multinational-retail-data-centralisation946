761a8e7923aeeae33f533e0454edc626a19ad29b
Emma Luk <emma_luk1@hotmail.com>
2023-12-05 17:33:20 +0000

    add credentials.yaml

--- Python/database_utils.py
+++ Python/database_utils.py
@@ -1,18 +1,25 @@
 import yaml
 import sqlalchemy as sa
 import psycopg2
+#from database_utils import DatabaseConnector
 
 class DatabaseConnector:
     def __init__(self):
         self.engine = None
 
-    def read_db_creds(self):
-        with open('db_creds.yaml') as f:
+    def read_db_creds(self, filename):
+        with open(filename, 'r') as f:
             creds = yaml.safe_load(f)
         return creds
+    # Read database credentials from db_creds.yaml
+    #db_creds = db_connector.read_db_creds('db_creds.yaml')
+    
+    # Initialize the database engine using the credentials
+    #engine = db_connector.init_db_engine(db_creds)
 
     def init_db_engine(self):
-        creds = self.read_db_creds()
+        #creds = self.read_db_creds()
+        creds = self.read_db_creds('credentials.yaml')  # Pass the filename explicitly
         engine_string = f'postgresql+psycopg2://{creds["RDS_USER"]}:{creds["RDS_PASSWORD"]}@{creds["RDS_HOST"]}:{creds["RDS_PORT"]}/{creds["RDS_DATABASE"]}'
         self.engine = sa.create_engine(engine_string)
         print(engine_string)
--- Python/main.py
+++ Python/main.py
@@ -1,10 +1,14 @@
+# main.py
 from data_extraction import DataExtractor
 from database_utils import DatabaseConnector
 from data_cleaning import DataCleaning
+import yaml
+from database_utils import DatabaseConnector
+
 # Extract data from RDS database
 data_extractor = DataExtractor()
 db_connector = DatabaseConnector()
-sourceDB = db_connector.init_db_engine()
+engine = db_connector.init_db_engine()
 
 # Get legacy users table name
 table_name = 'legacy_users'
@@ -15,15 +19,18 @@ legacy_users_data = data_extractor.extract_data_from_rds(table_name)
 # Print legacy users data
 print(legacy_users_data)
 
-# Extract data from RDS database
-# data_extractor = DataExtractor()
-# db_connector = DatabaseConnector()
-## table_name = 'user_data'
-## user_data = data_extractor.extract_data_from_rds(table_name)
-
 # Clean user data
 data_cleaning = DataCleaning()
 cleaned_user_data = data_cleaning.clean_user_data(legacy_users_data)
 
 # Upload cleaned data to database
-db_connector.upload_to_db(cleaned_user_data, 'dim_users', 'sales_data')
+db_connector.upload_to_db(cleaned_user_data, 'dim_users', engine=engine)  # Explicitly pass the engine object
+
+# Read database credentials from credentials.yaml
+db_creds = DatabaseConnector.read_db_creds('credentials.yaml')
+
+# Initialize the database engine using the credentials from the alternative file
+engine = DatabaseConnector.init_db_engine(db_creds)
+
+# Perform data extraction, transformation, and loading (ETL) operations
+
