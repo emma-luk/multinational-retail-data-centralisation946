229d6f331cbf61ee716a479ef47d94dcfc28fa3e
Emma Luk <emma_luk1@hotmail.com>
2023-12-05 21:51:49 +0000

    two seperate engine instances

--- Python/main.py
+++ Python/main.py
@@ -3,34 +3,26 @@ from data_extraction import DataExtractor
 from database_utils import DatabaseConnector
 from data_cleaning import DataCleaning
 import yaml
-from database_utils import DatabaseConnector
 
-# Extract data from RDS database
+# Initialize RDS engine using credentials from db_creds.yaml
+db_rds = DatabaseConnector()
+rds_creds = db_rds.read_db_creds('db_creds.yaml')  # Explicitly pass the filename
+rds_engine = DatabaseConnector.init_db_engine(rds_creds)
+
+# Initialize local database engine using credentials from local_credentials.yaml
+db_local = DatabaseConnector()
+local_creds = db_local.read_db_creds('local_credentials.yaml')  # Explicitly pass the filename
+local_engine = DatabaseConnector.init_db_engine(local_creds)
+
+# Extract data from RDS database using RDS engine
 data_extractor = DataExtractor()
-db_connector = DatabaseConnector()
-engine = db_connector.init_db_engine()
-
-# Get legacy users table name
 table_name = 'legacy_users'
-
-# Extract legacy users data
-legacy_users_data = data_extractor.extract_data_from_rds(table_name)
-
-# Print legacy users data
-print(legacy_users_data)
+legacy_users_data = data_extractor.extract_data_from_rds(table_name, rds_engine)
 
 # Clean user data
 data_cleaning = DataCleaning()
 cleaned_user_data = data_cleaning.clean_user_data(legacy_users_data)
 
-# Upload cleaned data to database
-db_connector.upload_to_db(cleaned_user_data, 'dim_users', engine=engine)  # Explicitly pass the engine object
-
-# Read database credentials from credentials.yaml
-db_creds = DatabaseConnector.read_db_creds('credentials.yaml')
-
-# Initialize the database engine using the credentials from the alternative file
-engine = DatabaseConnector.init_db_engine(db_creds)
-
-# Perform data extraction, transformation, and loading (ETL) operations
-
+# Upload cleaned data to local database using local engine
+db_connector = DatabaseConnector()
+db_connector.upload_to_db(cleaned_user_data, 'local_dim_users', engine=local_engine)
